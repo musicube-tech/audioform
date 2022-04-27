cd502b28eb920a6c59e385a1a8c0f78107bf6c1f
Xiphe <github@xiphe.net>
2022-04-27 18:09:00 +0200

    feat: try converting file to mp3 when initial decoding fails

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { AudioContext } from 'web-audio-api';
+import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
+import { spawn } from 'child_process';
+import { write, file } from 'tempy';
+import fs from 'fs/promises';
 
 export interface AudioBuffer {
   getChannelData(channel: number): number[];
@@ -24,11 +28,63 @@ export default async function audioToWaveformData(
   return normalizeData(filteredData(data, samples));
 }
 
-export function decodeAudio(buffer: Buffer): Promise<AudioBuffer> {
+export function decodeAudio(
+  buffer: Buffer,
+  mayConvert: boolean = true,
+): Promise<AudioBuffer> {
   const context: IAudioContext = new AudioContext();
   return new Promise<AudioBuffer>((resolve, reject) => {
-    context.decodeAudioData(buffer, resolve, reject);
+    context.decodeAudioData(buffer, resolve, (err) => {
+      if (mayConvert) {
+        tryConvert(buffer).then(resolve, reject);
+      } else {
+        reject(err);
+      }
+    });
   });
+}
+
+async function tryConvert(buffer: Buffer): Promise<AudioBuffer> {
+  let f: string;
+  let o: string;
+
+  try {
+    o = file({ extension: 'mp3' });
+    f = await write(buffer);
+
+    const ffmpeg = spawn(ffmpegPath, [
+      '-i',
+      f,
+      '-vn',
+      '-ar',
+      '44100',
+      '-ac',
+      '2',
+      '-b:a',
+      '192k',
+      o,
+    ]);
+    await new Promise((resolve, reject) => {
+      ffmpeg.on('error', reject);
+      ffmpeg.on('exit', resolve);
+    });
+    var out = decodeAudio(await fs.readFile(o), false);
+  } finally {
+    await Promise.all(
+      [f!, o!].map(
+        (t) =>
+          new Promise<void>((resolve) => {
+            if (t) {
+              fs.unlink(t).then(resolve, resolve);
+            } else {
+              resolve();
+            }
+          }),
+      ),
+    );
+  }
+
+  return out;
 }
 
 export function filteredData(
